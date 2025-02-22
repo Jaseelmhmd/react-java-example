abc5297b7a5769cf32f035505df523089d5748cc
Adhil Kv <159995868+aaadhlll@users.noreply.github.com>
2025-02-22 22:38:52 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Aura Dev</h1>
-        
+        <h2>Hi da Makkaleee..... Enjoymaadi</h2>
       </header>
     </div>
   );
