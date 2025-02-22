54d5ae215d0b55b9a2f6dbc1123e507e9c363ffb
Athilc01 <adilcheerothattil@gmail.com>
2025-02-22 22:44:32 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,8 @@ function App() {
         <h1>Aura Dev</h1>
         <h2>Hi da Makkaleee..... Enjoymaadi</h2>
         <h3>so Guys this is Our first Trial</h3>
+        <h4>Hope you all like it</h4>
+        <h5>Thank you</h5>
       </header>
     </div>
   );
